d7b47a2eefb4c8e2f14d2fc6347d2955364aef4e
Ayo <Ayo.meshe@gmail.com>
2024-03-26 16:12:50 +0000

    Adding Footer page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,28 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/Header';
-import Providers from './Providers';
-import Navbar from '@/components/Navbar';
-import SearchBox from '@/components/SearchBox';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Providers from "./Providers";
+import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'IMDb clone',
-  description: 'This is a movie database clone',
+  title: "IMDb clone",
+  description: "This is a movie database clone",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>
         <Providers>
           <Header />
           <Navbar />
           <SearchBox />
           {children}
+          <Footer />
         </Providers>
       </body>
     </html>
